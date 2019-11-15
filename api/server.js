8fd34f757611af4b3258102c76fb2c9883de384a
aaamg <andrewmanuelgarcia@gmail.com>
2019-11-15 16:31:34 -0500

    mvp

--- api/server.js
+++ api/server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
-const authenticate = require('../auth/authenticate-middleware.js');
-const authRouter = require('../auth/auth-router.js');
+//const authenticate = require('../auth/authenticate-middleware.js');
+const authenticate = require('../auth/restricted-middleware.js.js.js');
+const authRouter = require('../auth/auth-router.js.js.js');
 const jokesRouter = require('../jokes/jokes-router.js');
 
 const server = express();
@@ -16,3 +17,5 @@ server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 
 module.exports = server;
+
+//done
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -6,3 +6,5 @@
 module.exports = (req, res, next) => {
   res.status(401).json({ you: 'shall not pass!' });
 };
+
+//done
